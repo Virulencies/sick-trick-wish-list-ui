a3bcc64f81bbcf3caa85a94ea43c9471bc882c51
Theo McCray <tlmccraycray@gmail.com>
2024-05-28 10:29:04 -0600

    Finish iteration 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { fetchTricks } from './api/trickCalls';
+import { fetchTricks } from '../../api/trickCalls';
+import TrickList from '../TrickList';
 
 
+const App = () => {
+  const [tricks, setTricks] = useState([]);
 
-function App() {
+  useEffect(() => {
+    const getTricks = async () => {
+      try {
+        const data = await fetchTricks();
+        setTricks(data);
+      } catch (error) {
+        console.error('Error fetching tricks:', error);
+      }
+    };
+
+    getTricks();
+  }, []);
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
+      <TrickList tricks={tricks} />
     </div>
   );
-}
+};
 
-export default App; 
+export default App;
